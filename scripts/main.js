8d7491ed73b45703ee7e6f80f4f75a670953f9c9
Erica Thompson <younique98@gmail.com>
2021-01-19 13:37:32 -0800

    fixed conflict in main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,11 +1,8 @@
-<<<<<<< HEAD
 console.log("Welcome to the main module")
 import { PortugalCitiesList} from "./portugal/cities/citiesList.js"
 
 PortugalCitiesList()
-=======
 import { FranceCitiesList } from "./france/cities/CitiesList.js"
 
 FranceCitiesList()
 
->>>>>>> 8e1dc282368a056f52ac1cbd18c5ad5ce21da91d
