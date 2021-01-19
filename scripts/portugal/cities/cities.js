dbd39207774a64b46c7955f924fa2a350f43d225
Erica Thompson <younique98@gmail.com>
2021-01-19 11:46:20 -0800

    added string interpoalation to div classes

---
+++ scripts/portugal/cities/cities.js
@@ -0,0 +1,9 @@
+export const City = (cityObj) => {
+    return `
+        <section class="cityObj card">
+        <div><img  class="cityObj__image image--card" src="${cityObj.image}" /></div>            
+            <div class="cityObj__name">City: ${cityObj.name}</div>
+            <div class="CityObj__species">Population: ${cityObj.population}</div>
+        </section>
+    `
+}
