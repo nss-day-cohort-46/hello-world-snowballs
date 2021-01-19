2c403628f893b6ef014d47eef088d1b33c9b1862
Erica Thompson <younique98@gmail.com>
2021-01-19 09:51:59 -0800

    added images relative path for cities

---
+++ scripts/portugal/cities/citiesDataProvider.js
@@ -0,0 +1,22 @@
+const citiesCollection = [
+    {
+    name: "Name: Lisbon",
+    population: "Population: 510,000",
+    image: "images/portugal/lisbon-city-image.jpg"
+ 
+    },
+    {
+        name: "Name: Porto",
+        population: "Population: 240,000",
+        image: "images/portugal/Porto-city-image.jpg"
+     
+        },
+        {
+            name: "Name: Coimbra",
+            population: "Population: 105,842",
+            image: "images/portugal/coimbra-city-image.jpg"
+         
+            }
+            
+
+]
