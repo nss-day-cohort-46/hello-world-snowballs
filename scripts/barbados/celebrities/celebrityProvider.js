ac6c4d97a49db4034e885932ca8da0082f9613d9
TrentSisson <trent.c.sisson0@gmail.com>
2021-01-19 16:26:13 -0600

    filled out provider for celibrity

---
+++ scripts/barbados/celebrities/celebrityProvider.js
@@ -0,0 +1,50 @@
+const celebrityCollection =[
+    {
+        name:"Grandmaster Flash",
+        image:"images/grand-master-flash.jpg"
+
+    },
+    {
+        name:"Doug E Fresh",
+        image:"images/doug e fresh.jpg"
+    },
+    {
+        name:"Rihanna",
+        image:"images/rihanna-4.jpg"
+    }
+]
+export const useCelebrityCollection = () =>{
+return celebrityCollection.slice()
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//  <ul>
+//  <h2>Famous People</h2>
+//  <li>Grandmaster Flash</li>
+//  <li>Mobadele Thompson</li>
+//  <li>Doug E Fresh</li>
+//  <li>shontella</li>
+//  <li>Rihanna</li>
+
+
+// </ul>
+
+
+
+
+
+
