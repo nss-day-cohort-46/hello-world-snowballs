b5aecf6e055df6fecf84e587971b7562b0d2c60a
TrentSisson <trent.c.sisson0@gmail.com>
2021-01-19 13:47:43 -0600

    filled out city.js, created the city card for my cities

---
+++ scripts/barbados/cities/city.js
@@ -0,0 +1,9 @@
+export const City = (cityObj) =>{
+    return`
+    <section class="cityObjectCard">
+    <div class="city__name"> CITY: ${cityObj.name}</div>
+    <div class="city__population>POPULATION: ${cityObj.population}</div>
+    <div class="city__image"> src="${cityObj.image}"</div>
+
+    `
+}
