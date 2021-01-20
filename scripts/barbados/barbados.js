0f796ca06f01fdd22755b2243b8349d11343e09f
TrentSisson <trent.c.sisson0@gmail.com>
2021-01-20 09:31:46 -0600

    added landmarks and rendered them to the dom. created barbados.js

---
+++ scripts/barbados/barbados.js
@@ -0,0 +1,7 @@
+import {barbadosCities} from "./barbados/cities/cityList.js"
+import {barbadosCelebrities} from "./barbados/celebrities/celebrityList.js"
+import { barbadosLandmarks } from "./barbados/landmarks/landmarksList.js"
+
+barbadosCities()
+barbadosCelebrities()
+barbadosLandmarks()
