8388f6c7f0959c0695f61aa5639206f771b66990
Erica Thompson <younique98@gmail.com>
2021-01-20 07:37:54 -0800

    added portugal main js

--- scripts/main.js
+++ scripts/main.js
@@ -1,12 +1,10 @@
 import {IcelandCityList} from "./iceland/cities/citiesList.js"
 import {barbadosCities} from "./barbados/cities/cityList.js"
 import { FranceCitiesList } from "./france/cities/CitiesList.js"
-import { PortugalCitiesList} from "./portugal/cities/citiesList.js"
-
 
 
 IcelandCityList() 
-PortugalCitiesList()
+
 barbadosCities()
 FranceCitiesList()
 
