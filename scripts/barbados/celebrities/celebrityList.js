b60ed1c4860771d50aec41eed91c9a34b213df33
TrentSisson <trent.c.sisson0@gmail.com>
2021-01-19 22:27:30 -0600

    test

--- scripts/barbados/celebrities/celebrityList.js
+++ scripts/barbados/celebrities/celebrityList.js
@@ -6,7 +6,7 @@ import {Celebrity} from "./celebrity.js"
 export const barbadosCelebrities = () => {
 
     
-    // stores the targeted HTML id and stores it in a variable.
+    // stores the targeted HTML and stores it in a variable.
     const contentElement = document.querySelector(".barbados__bottomContainer__people")
 
     // stores the imported function into a variable
@@ -17,10 +17,10 @@ let barbadosCelebrityHTML = ""
 
     // runs a loop that converts objects in an array  to HTML and stores in the above variable 
 for (const peeps of famousPeeps ) {
-    barbadosCityHTML += Celebrity(peeps)
+    barbadosCelebrityHTML += Celebrity(peeps)
 }
 
-    // writes the loaded variable and  the HTML <h3> into the targeted HTML id
+    // writes the loaded variable and  the HTML <h3> into the targeted HTML
 contentElement.innerHTML += `
 <h3 style=color:blue;font-size:20px >Cities</h3>
 ${barbadosCelebrityHTML}
--- scripts/main.js
+++ scripts/main.js
@@ -1,12 +1,12 @@
-import {IcelandCityList} from "./iceland/cities/citiesList.js"
 import {barbadosCities} from "./barbados/cities/cityList.js"
 import { FranceCitiesList } from "./france/cities/CitiesList.js"
 import { PortugalCitiesList} from "./portugal/cities/citiesList.js"
-import {barbadosCelebrities} from "./barbados/celebrities/celebrityList.js"
+// import {barbadosCelebrities} from "./barbados/celebrities/celebrityList.js"
+import {IcelandCityList} from "./iceland/cities/citiesList.js"
 
-
-IcelandCityList() 
+IcelandCityList()
 PortugalCitiesList()
 barbadosCities()
 FranceCitiesList()
 barbadosCelebrities()
+
